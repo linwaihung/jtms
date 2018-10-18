f9ee4b9df852b9f227a24d58e0bc8d5766a66d5e
linwaihung <linwaihung@yahoo.com>
2018-10-18 22:24:30 +0800

    Third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 var obj = { name: "John", age: 30, city: "New York" };
 
 app.get('/', (request, response) => {
